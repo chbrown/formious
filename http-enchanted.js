d99efe9a83a138eedd8550d0d45e5859a22343c6
Christopher Brown <io@henrian.com>
2014-04-06 23:34:36 -0500

    Trim down http extras

--- http-enchanted.js
+++ http-enchanted.js
@@ -1,56 +1,13 @@
 /*jslint node: true */
 var http = require('http-enhanced');
-var url = require('url');
-var querystring = require('querystring');
-var util = require('util');
 var logger = require('loge');
-var amulet = require('amulet');
 
-http.ServerResponse.prototype.adapt = function(req, ctx, templates) {
-  // try to flexibly determine the desired content type
-  var content_type = 'html';
-  if (req.headers.accept.match(/application\/json/)) {
-    content_type = 'json';
+http.ServerResponse.prototype.die = function(error) {
+  if (this.statusCode == 200) {
+    this.statusCode = 500;
   }
-  else if (req.url.match(/.json$/)) {
-    content_type = 'json';
-  }
-
-  // okay, now adapt and respond accordingly
-  if (content_type == 'json') {
-    this.json(ctx);
-  }
-  else { // assume html
-    amulet.stream(templates, req.ctx).pipe(this);
-  }
-};
-
-http.ServerResponse.prototype.ngjson = function(http_status_code, obj) {
-  if (obj === undefined) {
-    obj = http_status_code;
-    http_status_code = 200;
-  }
-
-  var prefix = ")]}',\n";
-  var body = JSON.stringify(obj);
-  this.writeAll(http_status_code, 'application/json', prefix + body);
-};
-
-http.ServerResponse.prototype.die = function(http_code, err) { // function(/*, error_args... */) {
-  // if only one argument is specified, it must be the error string
-  // var error_args = Array.prototype.slice.call(arguments, 0);
-  // var err = util.format.apply(util, arguments);
-  if (err === undefined && isNaN(http_code)) {
-    err = http_code;
-    http_code = 500;
-  }
-
-  if (err) {
-    // var err_deep = util.inspect(err, {showHidden: true, depth: 2});
-    logger.error('res.die:', err.stack, err);
-  }
-  var str = err ? 'Failure: ' + err.toString() : 'Failure';
-  this.writeAll(http_code, 'text/plain', str);
+  logger.error('res.die:', error.stack, error);
+  return this.text(error ? 'Failure: ' + error.toString() : 'Failure');
 };
 
 module.exports = http;
