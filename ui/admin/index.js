f284e7cc634dcc65b5234bb303edf9ad61e5687d
Christopher Brown <io@henrian.com>
2015-04-22 11:50:43 -0500

    Add table detection to renderObject method

--- ui/admin/index.js
+++ ui/admin/index.js
@@ -1,13 +1,9 @@
 /*jslint browser: true */ /*globals _, mercury */
-
-var log = console.log.bind(console);
-
-// var document = require('global/document');
-// var hg = mercury;
 var h = mercury.h;
+var log = console.log.bind(console);
+var pushAll = function(array, xs) { return Array.prototype.push.apply(array, xs); };
 
 function renderObject(object) {
-  // log('renderObject(%o)', object);
   if (object === undefined) {
     return h('i.undefined', 'undefined');
   }
@@ -15,7 +11,36 @@ function renderObject(object) {
     return h('b.null', 'null');
   }
   else if (Array.isArray(object)) {
-    // TODO: check for tabular arrays (all of the items are objects with the same keys)
+    // check for tabular arrays (all of the items are objects with the same keys)
+    var items_are_objects = object.every(function(value) {
+      return (value !== null) && (value !== undefined) && (typeof value === 'object');
+    });
+    if (object.length > 0 && items_are_objects) {
+      // now check that all the keys are the same
+      var columns = Object.keys(object[0]);
+      var items_have_indentical_keys = object.slice(1).every(function(value) {
+        // log('eq %o = %o', value, columns);
+        return _.isEqual(Object.keys(value), columns);
+      });
+      if (items_have_indentical_keys) {
+        var thead_children = h('tr', columns.map(function(column) {
+          return h('th', column);
+        }));
+        var tbody_children = object.map(function(value) {
+          var cells = columns.map(function(column) {
+            return h('td', renderObject(value[column]));
+          });
+          return h('tr', cells);
+        });
+        return h('div.table', [
+          h('table', [
+            h('thead', thead_children),
+            h('tbody', tbody_children),
+          ])
+        ]);
+      }
+    }
+    // otherwise, it's an array of arbitrary objects
     var array_children = object.map(function(value) {
       return renderObject(value);
     });
