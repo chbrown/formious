09417ffd3d703538ebad7ff51be450b975b47b44
Christopher Brown <io@henrian.com>
2017-04-21 02:01:01 -0500

    Add pretty-printing table function and use for routes in dev

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,5 @@
 (ns user
-  (:require [clojure.pprint :refer [pp pprint print-table]]
+  (:require [pprint :refer [print-table]]
             [bidi.bidi :as bidi]
             [formious.common :as common]
             [formious.routes :as routes]
@@ -8,7 +8,7 @@
 
 (println "⭐ Loading /dev/user⭐")
 
-(defn print-routes
+(defn print-route-handlers
   "Print out the handler keywords for all the app's routes"
   []
   (->> (bidi/route-seq routes/routes)
@@ -17,6 +17,14 @@
        (sort)
        (map println)
        (dorun)))
+
+(defn print-routes
+  "Print out all the app's routes"
+  []
+  (->> (bidi/route-seq routes/routes)
+       (map (fn [route] (assoc (dissoc route :handler) :handler-*ns* (namespace (:handler route))
+                                                       :handler-name (name (:handler route)))))
+       (print-table)))
 
 (def aws-credentials {:access_key_id (System/getenv "AWS_ACCESS_KEY_ID")
                       :secret_access_key (System/getenv "AWS_SECRET_ACCESS_KEY")})
