b41b0da8ae625b8078045c61e276ab188e5252de
Christopher Brown <io@henrian.com>
2018-02-19 01:04:39 -0600

    Add Transit-format response middleware

--- src/formious/server/middleware.clj
+++ src/formious/server/middleware.clj
@@ -1,12 +1,12 @@
 (ns formious.server.middleware
-  (:require [formious.routes :as routes]
-            [formious.common :as common]
+  (:require [clojure.string :as str]
             [clojure.tools.logging :as log]
             [ring.util.request :as request]
-            [ring.util.response :as response]))
+            [ring.util.response :as response]
+            [formious.util :refer [assoc-in-when write-transit-str]]))
 
 (defn wrap-server-header
-  "middleware that adds a 'Server: <server>' header to the response"
+  "middleware that adds a 'Server' header to the response"
   [handler server]
   (fn server-header-handler [request]
     (response/header (handler request) "Server" server)))
@@ -20,25 +20,24 @@
       (log/trace "📬  (handling response)" ~@tags)
       response)))
 
-; cf. https://github.com/nberger/ring-logger
-
 (defn- redact-in
   [m ks]
-  (common/assoc-in-when m ks "❌ ")) ; maybe ❌ or … or ⋯
+  (assoc-in-when m ks "⋯ ")) ; maybe ❌ or … or ⋯
 
 (defn wrap-logging
-  "middleware that logs requests and responses"
+  "Middleware that logs requests and responses.
+  Cf. https://github.com/nberger/ring-logger"
   [handler & {:keys [tag request-redactions response-redactions]
-              :or   {request-redactions [[:remote-addr]
-                                         [:headers "user-agent"]
-                                         [:headers "cookie"]
-                                         [:headers "connection"]
-                                         [:headers "accept"]
-                                         [:headers "accept-language"]
-                                         [:headers "accept-encoding"]
-                                         [:async-channel]]
-                     response-redactions [[:body]
-                                          [:headers "Last-Modified"]]}
+              :or   {request-redactions  #{[:remote-addr]
+                                           [:headers "user-agent"]
+                                           [:headers "cookie"]
+                                           [:headers "connection"]
+                                           [:headers "accept"]
+                                           [:headers "accept-language"]
+                                           [:headers "accept-encoding"]
+                                           [:async-channel]}
+                     response-redactions #{[:body]
+                                           [:headers "Last-Modified"]}}
               :as options}]
   (fn logging-handler [request]
     (let [redacted-request (reduce redact-in request request-redactions)]
@@ -47,3 +46,17 @@
       (let [redacted-response (reduce redact-in response response-redactions)]
         (log/debug "📬  ⬅ (response)" tag redacted-response))
       response)))
+
+(defn wrap-transit-response
+  "Middleware to serialize :body in responses as transit format iff:
+  - (coll? :body) AND
+  - the request headers include 'Accept: application/transit+json'"
+  [handler & {:keys [mime-type]
+              :or   {mime-type "application/transit+json"}}]
+  (fn transit-response-handler [request]
+    (let [response (handler request)]
+      (if (and (coll? (:body response))
+               (str/includes? (get-in response [:headers "accept"]) mime-type))
+        (-> (update-in response [:body] write-transit-str)
+            (response/content-type mime-type))
+        response))))
