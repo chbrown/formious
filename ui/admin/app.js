ccc0237989adb3221024da58daab1e77505343ba
Christopher Brown <io@henrian.com>
2018-03-08 12:19:08 -0600

    Move 'misc-js-plugins' directives directly into 'app'

--- ui/admin/app.js
+++ ui/admin/app.js
@@ -16,19 +16,95 @@ import 'flow-copy'
 import 'checkbox-sequence'
 import 'textarea'
 
-// for the misc-js-plugins module below. TODO: factor out
-import './misc-js-plugins'
-
 export var app = angular.module('app', [
   'ngResource',
   'ngStorage',
   'ui.router',
   'ngUpload',
-  'misc-js-plugins', // TODO: factor this out
   'flow-copy', // exposes an EA 'fixedflow' directive
   'checkbox-sequence', // exposes an A 'checkbox-sequence' directive
   'textarea',
 ])
+
+/** Copyright 2012-2014, Christopher Brown <io@henrian.com>, MIT Licensed
+https://raw.github.com/chbrown/misc-js/master/angular-plugins.js */
+app.directive('help', function() {
+  /**
+  <span class="help">This is a long but very useful help message but most often you probably won't want to read all of it because it's actually easy to remember and not all that helpful. Good for reference though.</span>
+
+  This message will be truncated to the first 50 characters (TODO: make that configurable).
+  */
+  return {
+    restrict: 'C',
+    template:
+      '<span class="summary" style="opacity: 0.5" ng-hide="expanded" ng-click="expanded = true"></span>' +
+      '<span class="full" ng-show="expanded" ng-click="expanded = false" ng-transclude></span>',
+    transclude: true,
+    scope: {},
+    link: function(scope, el) {
+      var summary_el = el.children().eq(0)
+      var full_el = el.children().eq(1)
+      scope.expanded = false
+
+      // var content = el.text().trim()
+      var content = full_el.text().trim()
+      var summary = content.slice(0, 50) + ((content.length > 50) ? '...' : '')
+      summary_el.text(summary)
+    },
+  }
+})
+app.directive('jsonTransform', function() {
+  /** parser/serializer to edit a JSON object within a textarea or input[type=text] */
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, el, attrs, ngModel) {
+      // set up communicating from DOM to model
+      el.on('blur keyup change', function() {
+        scope.$apply(function() {
+          ngModel.$setViewValue(el.val())
+          // if we wanted to read from page's html before from the model, we'd
+          // run this function at the link level (but usually we want the model)
+        })
+      })
+      // set up communicating from model to DOM
+      ngModel.$render = function() {
+        el.val(ngModel.$viewValue)
+        // this would trigger a textarea to resizeToFit, for example. kind of awkward, though.
+        el[0].dispatchEvent(new Event('input'))
+      }
+
+      // set up translations
+      ngModel.$formatters.push(function(value) {
+        if (value === null) {
+          return ''
+        }
+        // signature: angular.toJson(obj, [pretty])
+        return angular.toJson(value, true)
+      })
+      ngModel.$parsers.push(function(stringValue) {
+        // we'll interpret the empty string as 'null'
+        var value
+        if (stringValue === '') {
+          value = null
+        }
+        else {
+          value = stringValue
+        }
+
+        try {
+          ngModel.$setValidity('json', true)
+          // angular uses an unwrapped JSON.parse, so it'll throw on failure
+          return angular.fromJson(value)
+        }
+        catch (exc) {
+          ngModel.$setValidity('json', false)
+          // return undefined
+        }
+      })
+    },
+  }
+})
 
 function renderObject(object) {
   // console.log('renderObject', object)
