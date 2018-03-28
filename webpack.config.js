0a73e670ac579dc070fc32bbccd10126b73ef153
Christopher Brown <io@henrian.com>
2018-03-28 11:23:32 -0500

    Fix webpack config formatting and remove unused 'webpack' import

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path')
-const webpack = require('webpack')
 
 const env = process.env.NODE_ENV || 'development'
 
 module.exports = {
   mode: env,
   // 'babel-polyfill' is required for generators
-  entry: ['babel-polyfill',
-          './ui/admin/app',
-          // also, import all the app components
-          './ui/admin/models',
-          './ui/admin/access_tokens/controllers',
-          './ui/admin/administrators/controllers',
-          './ui/admin/aws_accounts/controllers',
-          './ui/admin/experiments/controllers',
-          './ui/admin/experiments/blocks/controllers',
-          './ui/admin/mturk/controllers',
-          './ui/admin/responses/controllers',
-          './ui/admin/templates/controllers'],
+  entry: [
+    'babel-polyfill',
+    './ui/admin/app',
+    // also, import all the app components
+    './ui/admin/models',
+    './ui/admin/access_tokens/controllers',
+    './ui/admin/administrators/controllers',
+    './ui/admin/aws_accounts/controllers',
+    './ui/admin/experiments/controllers',
+    './ui/admin/experiments/blocks/controllers',
+    './ui/admin/mturk/controllers',
+    './ui/admin/responses/controllers',
+    './ui/admin/templates/controllers',
+  ],
   output: {
     path: path.resolve(__dirname, 'ui', 'build'),
     filename: 'bundle.js',
