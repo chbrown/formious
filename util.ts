aa34b596c8ad97c78c02ed81b31ec948046d98c1
Christopher Brown <io@henrian.com>
2018-04-17 13:14:39 -0500

    Rename salt -> secret to reflect multiple uses

--- util.ts
+++ util.ts
@@ -4,27 +4,27 @@ import {createHash, createCipher} from 'crypto'
 This constant was created by calling `openssl rand -hex 32`.
 If it's ever used, a warning is generated and logged.
 */
-const defaultSalt = Buffer.from('06163e5e76046e13012a916e00fcd8e25836a97a671aa72eb5293ad51aa7e737', 'hex')
+const defaultSecret = Buffer.from('06163e5e76046e13012a916e00fcd8e25836a97a671aa72eb5293ad51aa7e737', 'hex')
 
-export function readSalt(environmentVariable: string = 'FORMIOUS_SALT'): Buffer {
+function readSecret(environmentVariable: string = 'FORMIOUS_SECRET'): Buffer {
   if (environmentVariable in process.env) {
-    const saltString = process.env[environmentVariable]
-    const encoding = /^[0-9A-Fa-f]+$/.test(saltString) ? 'hex' : 'utf8'
-    return Buffer.from(saltString, encoding)
+    const envString = process.env[environmentVariable]
+    const encoding = /^[0-9A-Fa-f]+$/.test(envString) ? 'hex' : 'utf8'
+    return Buffer.from(envString, encoding)
   }
-  console.warn(`Security warning: the "${environmentVariable}" environment variable was not set -- using default salt!`)
-  return defaultSalt
+  console.warn(`Security warning: the "${environmentVariable}" environment variable was not set -- using default secret!`)
+  return defaultSecret
 }
 
 export function sha256(data: string, encoding: 'utf8' | 'ascii' | 'latin1' = 'utf8'): string {
   const hash = createHash('sha256')
-  hash.update(readSalt())
+  hash.update(readSecret())
   hash.update(data, encoding)
   return hash.digest('hex')
 }
 
 export function blowfish(data: string, encoding: 'utf8' | 'ascii' | 'binary' = 'utf8'): string {
-  const cipher = createCipher('bf-cbc', readSalt())
+  const cipher = createCipher('bf-cbc', readSecret())
   const prefix = cipher.update(data, encoding)
   const suffix = cipher.final()
   const encrypted = Buffer.concat([prefix, suffix])
